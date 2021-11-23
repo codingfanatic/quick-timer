6cffa8efb801a2583e6cd3624b0efc23ea19f1b1
codingfanatic <codingfanatic@github.com>
2021-11-23 02:06:52 -0500

    Sample code from Android website. Ignore for now.

--- QuickTimer/app/src/main/java/com/codingfanatic/quicktimer/MainActivity.kt
+++ QuickTimer/app/src/main/java/com/codingfanatic/quicktimer/MainActivity.kt
@@ -2,10 +2,26 @@ package com.codingfanatic.quicktimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        object : CountDownTimer(30000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                //mTextField.setText("seconds remaining: " + millisUntilFinished / 1000)
+            }
+
+            override fun onFinish() {
+                //mTextField.setText("done!")
+            }
+        }.start()
+
+
     }
 }
+
+
